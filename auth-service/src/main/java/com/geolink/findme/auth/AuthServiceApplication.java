5dfc7e6b01243523f3d7aa7fd250ef7423940447
bakandalex <bakandealex@gmail.com>
2026-08-14 11:12:17 +0100

    feat(auth): application principale et configuration applicative

---
+++ auth-service/src/main/java/com/geolink/findme/auth/AuthServiceApplication.java
@@ -0,0 +1,17 @@
+package com.geolink.findme.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
+
+
+
+
+
