9a0d710bb5288161035a1590c9484a74119fe5c4
bakandalex <bakandealex@gmail.com>
2026-08-25 22:08:57 +0100

    test(it): migrer les tests d'integration vers Jackson 3 et fiabiliser AddressCrudIT
    AddressCrudIT forgeait des JWT pour des UUID jamais persistes, ce qui viole
    la contrainte addresses.user_id -> users(id) introduite lors de la fusion
    3-tiers (409 DATA_INTEGRITY_VIOLATION au lieu du 201/409 attendu). Les
    scenarios qui creent une adresse font desormais une vraie inscription
    (signupToken()), a l'image du contournement deja utilise dans
    SupportAndRbacIT ; bearer() reste valable pour les appels en lecture seule.
    
    Ajoute aussi le seed du compte agent support (V13) et docker-java.properties
    (fixe l'API Docker a 1.41 pour Testcontainers) qui manquaient au suivi git.

--- src/test/java/com/geolink/findme/presentation/web/AuthFlowIT.java
+++ src/test/java/com/geolink/findme/presentation/web/AuthFlowIT.java
@@ -1,7 +1,7 @@
 package com.geolink.findme.presentation.web;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import com.geolink.findme.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/geolink/findme/presentation/web/SupportAndRbacIT.java
+++ src/test/java/com/geolink/findme/presentation/web/SupportAndRbacIT.java
@@ -1,6 +1,6 @@
 package com.geolink.findme.presentation.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import com.geolink.findme.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
